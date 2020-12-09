5a51895280e73e82765ffbad1596a71473281ba6
AdaraDiphda23 <59851100+AdaraDiphda23@users.noreply.github.com>
2020-12-09 05:21:11 +0000

    Create CF920-Div2-B.cpp
    B - Tea Queue

---
+++ Codeforces/CF920-Div2-B.cpp
@@ -0,0 +1,17 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main(){
+        int t,n,l,r,ans;
+        cin>>t;
+        for (int i=0; i<t; i++){
+             cin>>n;
+             for(int k=1, j=1; j<=n; k++,j++){
+             	cin>>l>>r;
+            	if (l>k) k=l;
+                (k<l || k>r)? k--, cout<<0<<" ":cout<<k<<" ";
+             }
+             cout<<endl;
+        }
+        return 0;
+    }
